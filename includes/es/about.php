7747d2d310e7ae822a3d7683992c68d0bafa4947
Marco Antonio Angel Galeana <marcoangelgaleana@outlook.com>
2023-11-16 19:49:05 -0500

    se modifico el about y education

--- includes/es/about.php
+++ includes/es/about.php
@@ -6,15 +6,15 @@
             <span class="text-primary">ANGEL</span>
         </h1>
         <div class="subheading mb-5">
-            Playa del Carmen · México · (998) 810-9491 ·
+            Playa del Carmen · México · <a href="tel:9988109491">(998) 810-9491</a> ·
             <a href="mailto:marcoangelgaleana@outlook.com">marcoangelgaleana@outlook.com</a>
         </div>
-        <p class="lead mb-5">5 años de experiencia en el área de telecomunicaciones y redes, con habilidades de seguridad informática, programación en distintas plataformas y cuento con los certificados que hablan dichas habilidades.</p>
+        <p class="lead mb-5">5 años de experiencia en el área de telecomunicaciones y redes, con habilidades de seguridad informática, 
+            programación en distintas plataformas y cuento con los certificados que avalan dichas habilidades.</p>
         <div class="social-icons">
-            <a class="social-icon" href="https://www.linkedin.com/in/marco-angel-galeana-887998b2/"><i class="fab fa-linkedin-in"></i></a>
-            <a class="social-icon" href="#!"><i class="fab fa-github"></i></a>
-            <a class="social-icon" href="https://twitter.com/marcoangelgalea"><i class="fab fa-twitter"></i></a>
-            <a class="social-icon" href="https://www.facebook.com/angelgaleana/"><i class="fab fa-facebook-f"></i></a>
+        <a class="social-icon" href="https://www.linkedin.com/in/marco-antonio-angel-galeana-887998b2/" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+            <a class="social-icon" href="https://github.com/marcoangel/" target="_blank"><i class="fab fa-github"></i></a>
+            <a class="social-icon" href="https://twitter.com/marcoangelgalea" target="_blank"><i class="fab fa-twitter"></i></a>
         </div>
     </div>
     </section>
--- includes/es/education.php
+++ includes/es/education.php
@@ -7,6 +7,9 @@
                 <h3 class="mb-0">Universidad del Caribe, cancun</h3>
                 <div class="subheading mb-3">Ingeniería en Telemática </div>
                 <div>Tecnologías de la información - Telecomunicaciones </div>
+                <div>Durante mi formación académica, adquirí habilidades sólidas en redes de computadoras, 
+                    seguridad informática, protocolos de comunicación y desarrollo de software. Estoy capacitado para abordar desafíos en el ámbito de las tecnologías de la 
+                    información y las comunicaciones, contribuyendo a soluciones innovadoras y eficientes en el campo de la telemática.</div>
             </div>
             <div class="flex-shrink-0"><span class="text-primary">Agosto 2011 - Agosto 2015</span></div>
         </div>
@@ -15,8 +18,12 @@
                 <h3 class="mb-0">Universidad Internacional de La Rioja en México </h3>
                 <div class="subheading mb-3">Máster en seguridad informática</div>
                 <div>Seguridad informática - Análisis de riesgos  </div>
+                <div>A lo largo de mi programa de maestría, adquirí conocimientos avanzados en áreas como seguridad de redes, encriptación, hacking ético, gestión de 
+                    riesgos y forense digital. Esta experiencia me capacita para desarrollar e implementar medidas sólidas de ciberseguridad, garantizando la integridad, 
+                    confidencialidad y disponibilidad de la información digital. Estoy familiarizado con las normas y mejores prácticas más recientes de la industria, 
+                    preparado para abordar los desafíos en constante evolución del panorama de la ciberseguridad.</div>
             </div>
-            <div class="flex-shrink-0"><span class="text-primary">Abril 2021 - Actual</span></div>
+            <div class="flex-shrink-0"><span class="text-primary">Abril 2021 - Abril 2023</span></div>
         </div>
     </div>
 </section>
